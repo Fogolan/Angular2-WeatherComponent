09dca28771da2538da3a2d94825bd602c381bb9d
ITRANSITION\y2.bogdan <y2.bogdan@itransition.com>
2017-07-19 15:44:35 +0300

    added google api geocoding constants

--- app/weather-widget/constants/constants.js
+++ app/weather-widget/constants/constants.js
@@ -1,4 +1,6 @@
 "use strict";
 exports.FORECAST_KEY = "94b1b491a8e695a4fd5d51e58a427bab";
 exports.FORECAST_ROOT = "https://api.darksky.net/forecast/";
+exports.GOOGLE_KEY = "AIzaSyBG-IhLKuWtcPj9i82Q1KNmg-gi3nnQ6Jo";
+exports.GOOGLE_ROOT = "https://maps.googleapis.com/maps/api/geocode/json";
 //# sourceMappingURL=constants.js.map
--- app/weather-widget/constants/constants.ts
+++ app/weather-widget/constants/constants.ts
@@ -1,2 +1,4 @@
 export const FORECAST_KEY = "94b1b491a8e695a4fd5d51e58a427bab";
 export const FORECAST_ROOT = "https://api.darksky.net/forecast/";
+export const GOOGLE_KEY = "AIzaSyBG-IhLKuWtcPj9i82Q1KNmg-gi3nnQ6Jo";
+export const GOOGLE_ROOT = "https://maps.googleapis.com/maps/api/geocode/json";
